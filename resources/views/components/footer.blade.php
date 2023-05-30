e81a09b048a12f330393f9ab2668fe7a3417c716
unknown <paolagrillo127@gmail.com>
2023-05-30 16:57:41 +0200

    modifiche grafiche

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
     <div id="footer" class="container-fluid footer mt-auto py-3" data-bs-theme="dark">
         <div class="row">
             <div class="column-12">
-            <p class="paddingFooter text-white text-center fs-2">sito creato da Marco Omazzi</p>
+            <p class="paddingFooter text-white text-center fs-2">sito creato da Paola Grillo</p>
         </div>
         </div>
     </div>
